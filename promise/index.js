59279805ffcfdf7a438e6966820e1ea76130df70
LPieces <Ljh_Pieces@163.com>
2021-10-12 16:12:51 +0800

    提交

--- promise/index.js
+++ promise/index.js
@@ -1,13 +1,13 @@
 
 // 异步程序
-const data = $.ajax({
-    url: "http://localhost:3000/data.json",
-    async: false // 同步
-});
+// const data = $.ajax({
+//     url: "http://localhost:3000/data.json",
+//     async: false // 同步
+// });
 
-console.log(data.responseJSON.map(item=>item.name));
+// console.log(data.responseJSON.map(item=>item.name));
 
-console.log("My name is LPieces.");
+// console.log("My name is LPieces.");
 
 // const p = new Promise((resolve, reject) => {
 //     $.ajax({
@@ -17,8 +17,30 @@ console.log("My name is LPieces.");
 //         }
 //     })
 // })
-
 // p.then(res => {
 //     console.log(res.map(item=>item.name));
 // })
 // console.log("My name is LPieces.");
+
+function getData(){
+    return new Promise((resolve, reject) => {
+        // 注：当然 ajax 本身也有 Promise , 这里只是举个栗子
+        $.ajax({
+            url: 'http://localhost:3000/data.json',
+            success (data) {
+                resolve(data);
+            }
+        })
+    })
+}
+async function doSth(){
+    try{
+        const data = await getData();
+        console.log(data.map(item=>item.name));
+        console.log("My name is LPieces.");
+    }catch(err){
+        console.log(err);
+    }
+}
+doSth();
+console.log("My name is LPieces.");
